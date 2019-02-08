3a1b622f0f88f068394672de88c8fc7a7a5bd2d3
Nicolasdevsgi <nicoasdevsgi1998@gmail.com>
2019-02-08 01:36:44 -0200

    Calculadora pronta

---
+++ Nick.Calculadora/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Nick.Calculadora
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resultado:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Numero 2:";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(92, 59);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(53, 22);
+            this.txtNumero1.TabIndex = 4;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(90, 100);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(53, 22);
+            this.txtNumero2.TabIndex = 5;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(90, 142);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(53, 22);
+            this.txtResultado.TabIndex = 6;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(211, 58);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 7;
+            this.btnAdicionar.Text = "+";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.BotaoAdicionarClick);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(211, 145);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 23);
+            this.btnDividir.TabIndex = 8;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.BotaoDividirClick);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(211, 116);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.TabIndex = 9;
+            this.btnMultiplicar.Text = "x";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.BotaoMultiplicarClick);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Location = new System.Drawing.Point(211, 87);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtrair.TabIndex = 10;
+            this.btnSubtrair.Text = "-";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.BotaoSubtrairClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(326, 263);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnSubtrair;
+    }
+}
+
---
+++ Nick.Calculadora/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Nick.Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BotaoAdicionarClick(object sender, EventArgs e)
+        {
+            double numero1 = double.Parse(txtNumero1.Text);
+            double numero2 = double.Parse(txtNumero2.Text);
+            double resultado;
+
+            resultado = numero1 += numero2;
+            txtResultado.Text = resultado.ToString();
+
+        }
+
+        private void BotaoSubtrairClick(object sender, EventArgs e)
+        {
+            double numero1 = double.Parse(txtNumero1.Text);
+            double numero2 = double.Parse(txtNumero2.Text);
+            double resultado;
+
+            resultado = numero1 -= numero2;
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void BotaoDividirClick(object sender, EventArgs e)
+        {
+            double numero1 = double.Parse(txtNumero1.Text);
+            double numero2 = double.Parse(txtNumero2.Text);
+            double resultado;
+
+            resultado = numero1 /= numero2;
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void BotaoMultiplicarClick(object sender, EventArgs e)
+        {
+            double numero1 = double.Parse(txtNumero1.Text);
+            double numero2 = double.Parse(txtNumero2.Text);
+            double resultado;
+
+            resultado = numero1 *= numero2;
+            txtResultado.Text = resultado.ToString();
+        }     
+    }
+}
+
+
